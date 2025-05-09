b5388e51bff166aa63e658cce9ae9429e7c91eac
Vaibhav Mohankumar <vaibhavmohan500@gmail.com>
2025-05-09 16:41:55 -0500

    Initial Commit

---
+++ Factorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Factorer
+//
+//  Created by Vaibhav Mohankumar on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Factorer/FactorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactorerApp.swift
+//  Factorer
+//
+//  Created by Vaibhav Mohankumar on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FactorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
